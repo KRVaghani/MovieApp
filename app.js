1120c70def20cc0bab7a47cf436f54eecc9ec546
KRVaghani <kaushik.vaghani111@gmail.com>
2021-01-31 23:42:47 +0530

    comment

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.get('/results',(req,res)=>{
             console.log('error');
         }
         let data = JSON.parse(body);
-        res.render('movies', {data:data, searchQuery:query});
+        res.render('movies', {data:data, searchQuery:query}); // movie ejb file is loaded
         
     }); 
 })
 
 app.get('/search',(req,res)=>{
-    res.render('search');
+    res.render('search'); // for this search ejb file is loaded
 });
 
 
